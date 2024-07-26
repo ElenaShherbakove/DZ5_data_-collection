c6d23ba9671b8cdaf589cb624303a53a4d497408
Elena <Shherbakove@yandex.ru>
2024-07-26 23:06:48 +0300

    домашка 5 семинар

--- DZ5/spiders/countries.py
+++ DZ5/spiders/countries.py
@@ -4,39 +4,41 @@ import scrapy
 class CountriesSpider(scrapy.Spider):
     name = "countries"
     allowed_domains = ["finance.yahoo.com"]
-    start_urls = ["https://finance.yahoo.com/trending-tickers/?guccounter=1&guce_referrer=aHR0cHM6Ly9lZHUubGl2ZWRpZ2l0YWwuc3BhY2Uv&guce_referrer_sig=AQAAADGHJGtm3-4AUeC5nljuKtS_fiU1h8Qp4xlyIwrpiyYKPxo59E-QsLol8eJ5Mws0P0mUew9a19MP_dXQTFJVPLDag86uVKwQ5iWUyOSFdinelpbQ_QfSFm5YHaSH6aNhcbprdYXBw2GaKrxdYTFQFm7HivjPfyjOHUUNJR6J2YOb/"]
+    start_urls = ["https://finance.yahoo.com/trending-tickers"]
 
     def parse(self, response):
-        countries = response.xpath("//td/a")
+        countries = response.xpath("//table/tbody/tr")
         for country in countries:
-            name = country.xpath(".//text()").get()
+            name = country.xpath(".//td[1]/a/text()").get()
             link = country.xpath(".//@href").get()
-            yield{
+            yield(
                 #'country_name': name,
                 #'link': link
                 #scrapy.Request(url=link)
-                response.follow(url=link, callback= self.parse_country, meta={'country_name': name})
-            }
+                response.follow(url=link, callback= self.parse_country, meta={'name': name})
+            )
     def parse_country(self, response):
-        rows = response.xpath("//tr[contains(@class, 'yf-ewueuo')]")
+        name = response.request.meta['name']
+        rows = response.xpath("//*[@id='nimbus-app']/section/section/section/article/section[1]")
         for row in rows:
-            date = row.xpath(".//td/text()").get()
-            open = float(row.xpath(".//td[2]/text()").get())
-            high = float(row.xpath(".//td[3]/text()").get())
-            low = float(row.xpath(".//td[4]/text()").get())
-            close = float(row.xpath(".//td[5]/text()").get())
-            adj_close = float(row.xpath(".//td[6]/text()").get())
-            volume = float(row.xpath(".//td[7]/text()").get())
-            name = response.request.meta['country_name']
+            #date = row.xpath(".//td/text()").get().strip()
+            #open = row.xpath(".//td[2]/text()").get()
+            #high = row.xpath(".//td[3]/text()").get()
+            #low = row.xpath(".//td[4]/text()").get()
+            #close = row.xpath(".//td[5]/text()").get()
+            #adj_close = row.xpath(".//td[6]/text()").get()
+            #volume = row.xpath(".//td[7]/text()").get()
+            name_full = row.xpath(".//h1/text()").get()
 
             yield{
-                'country_name': name,
-                'date': date,
-                'open': open,
-                'high': high,
-                'low': low,
-                'close': close,
-                'adj_close': adj_close,
-                'volume': volume
+                'name': name,
+                'name_full': name_full
+                #'date': date,
+                #'open': open,
+                #'high': high,
+                #'low': low,
+                #'close': close,
+                #'adj_close': adj_close,
+                #'volume': volume
             }
 
